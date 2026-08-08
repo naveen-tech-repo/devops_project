ce84d89decb68de2b2c331d292ed51e9a8599292
naveen-tech-repo <nrohilla245@gmail.com>
2026-08-08 23:35:18 +0530

    Add comment to Product.java
    Added a comment indicating this is the product service.

--- product-service/src/main/java/com/ecommerce/product/model/Product.java
+++ product-service/src/main/java/com/ecommerce/product/model/Product.java
@@ -7,7 +7,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
-
+//this is product service
 @Document(collection = "products")
 public class Product {
 
